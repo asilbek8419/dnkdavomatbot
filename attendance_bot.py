6f3f1760f1d07a0c163a70e0cceb636bf90dd8ac
Asilbek Normatov <asilbek84@bk.ru>
2024-09-24 17:06:02 +0500

    obnovil vremya dlya opozdaniya2

--- attendance_bot.py
+++ attendance_bot.py
@@ -81,7 +81,7 @@ async def handle_late_response(update: Update, context: CallbackContext) -> None
 # Функция для получения таблицы за текущий день
 async def get_today_table(update: Update, context: CallbackContext) -> None:
     now = datetime.now()
-    today = now.date(TASHKENT_TZ)
+    today = now.date()
     
     # Фильтрация записей по текущей дате
     today_attendance = attendance[attendance['Check-in Time'].dt.date == today]
